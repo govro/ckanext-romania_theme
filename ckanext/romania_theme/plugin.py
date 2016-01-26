471eaf21d09b90f4f36777d40ac2616ce2034819
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-26 10:48:57 +0200

    Add before_update from IMapper

--- ckanext/romania_theme/plugin.py
+++ ckanext/romania_theme/plugin.py
@@ -1,3 +1,8 @@
+from datetime import datetime, timedelta
+
+from sqlalchemy import event
+
+import ckan.lib.helpers as h
 import ckan.model as model
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
@@ -31,3 +36,14 @@ class Romania_ThemePlugin(plugins.SingletonPlugin):
     def before_create(self, context, resource):
         if ('upload' in resource) and (type(resource['upload']) is not unicode) and (resource['upload'].type in ['application/pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/msword']):
             raise toolkit.ValidationError(['Fisierele de tip PDF, DOC sau DOCX nu sunt permise.'])
+
+    # IMapper
+    def before_update(self, mapper, connection, instance):
+        if instance.state == model.State.DELETED and instance.type in ['dataset', 'resource']:
+            # TODO @palcu: remove hardcoded value
+            if datetime.now() - instance.metadata_created > timedelta(seconds=10):
+                h.flash_error('Nu este permisa stergerea datelor la mai mult de 5 zile dupa data incarcarii, deoarece aceste date au intrat in circuitul public.')
+                return EXT_STOP
+
+    def after_update(self, mapper, connection, instance):
+        pass
