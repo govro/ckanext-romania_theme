467006ffa0a1045d632dd85c14b631be322ad5b7
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-04-02 01:33:36 +0300

    Add first test #11

--- ckanext/romania_theme/tests/test_plugin.py
+++ ckanext/romania_theme/tests/test_plugin.py
@@ -1,5 +1,27 @@
-"""Tests for plugin.py."""
-import ckanext.romania_theme.plugin as plugin
+import webtest
+import paste.fixture
+import pylons.test
 
-def test_plugin():
-    pass
+import ckan.model as model
+import ckan.tests.helpers as helpers
+import ckan.config
+import ckan.plugins
+
+
+class TestRomaniaTheme(helpers.FunctionalTestBase):
+    @classmethod
+    def setup_class(cls):
+        cls.app = paste.fixture.TestApp(pylons.test.pylonsapp)
+        ckan.plugins.load('romania_theme')
+
+    def teardown(self):
+        model.repo.rebuild_db()
+
+    @classmethod
+    def teardown_class(cls):
+        ckan.plugins.unload('romania_theme')
+
+    def test_motto(self):
+        response = self.app.get('/')
+        motto = 'Date accesibile, reutilizabile, ce pot fi redistribuite'
+        assert motto in response.body
